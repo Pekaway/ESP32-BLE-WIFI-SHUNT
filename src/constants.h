ccaa5e505fd99fd0e2e59e5c84221da56af3f527
Willi Preuk <willi@preuk.eu>
2025-05-28 17:24:42 +0200

    update version constant

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-#define VERSION 200
+// Set version to either 200A or 500A
+#define VERSION 500
 
 // BLE UUIDs
 auto constexpr SERVICE_UUID = "91bad492-b950-4226-aa2b-4ede9fa42f59";
